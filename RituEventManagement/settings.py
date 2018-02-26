f94b1ab91ee8f35e86982fdcd7e544d6ce25ca5c
ParadoxZero <sidhin.thomas@gmail.com>
2018-02-26 23:41:11 +0530

    integrated Celery to send messages async. Email images attached.

--- RituEventManagement/settings.py
+++ RituEventManagement/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+from config import EMAIL_USER, EMAIL_PASSWORD
+
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
@@ -121,3 +123,9 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 TEMPLATE_DIRS = (os.path.join(BASE_DIR,  'templates'),)
+
+EMAIL_HOST = "smtp.zoho.com"
+EMAIL_PORT = "587"
+EMAIL_USE_TLS = True
+EMAIL_HOST_PASSWORD = EMAIL_PASSWORD
+EMAIL_HOST_USER = EMAIL_USER
